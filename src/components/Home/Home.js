f120d6fb14f3fe564b451d39e9cae18724e4e3d1
subrata sarker <tccdcc8@gmail.com>
2022-04-04 02:11:11 +0600

    banner section added in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Home.css'
+
 
 const Home = () => {
     return (
-        <div>
-            <h2>this is home page</h2>
+        <div className='container'>
+            <div className='banner-info'>
+                <h1>Support of Practice</h1>
+                <h2> Why buy a book <br />
+                    Please Buy a future </h2>
+            </div>
+            <div className='banner-img'>
+                <img src="images/book.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/components/Notffound/Notfound.js
+++ src/components/Notffound/Notfound.js
@@ -5,8 +5,8 @@ import './Notfound.css'
 const Notfound = () => {
     return (
         <div>
-            <h1>404</h1>
-            <h1>Oppps Data Not Found !!!</h1>
+            <h1 className='notfoundheading'>404</h1>
+            <h1 className='notfoundheading'>Oppps Data Not Found !!!</h1>
             <h3>Please go to Home page</h3>
             <Link className='homebtn' to='/'>Home</Link>
         </div>
